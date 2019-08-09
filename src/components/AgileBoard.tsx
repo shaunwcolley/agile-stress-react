1c151806a3eff19337be8f24244bc01d42f7f9b6
Shaun Colley <swc7838@gmail.com>
2019-08-08 22:41:10 -0500

    initial commit

---
+++ src/components/AgileBoard.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface IProps {
+
+}
+
+const AgileBoard: React.FC<IProps> = (props) => {
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AgileBoard;
